9e79df2777955c25c5646d2eecef3a0c0aec02a3
João Victor da Silva Sales <joao60651@gmail.com>
2024-01-31 10:00:03 -0300

    corrige erro apagar foto nos croll

--- scripts/scrollReveal.js
+++ scripts/scrollReveal.js
@@ -2,7 +2,7 @@ ScrollReveal().reveal('.galery-about__legend', {
     delay: 15, 
     distance: '10px',
 });
-ScrollReveal().reveal('.galery-about__container-image', { 
+ScrollReveal().reveal('.galery-about__container-img', { 
     delay: 10, 
     distance: '50px',
     reset: true
